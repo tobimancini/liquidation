3d1785724deed264fec5b0e4580dfb61b6d765a5
Tobi Mancini <tobias_mancini@hotmail.com>
2024-02-13 01:36:49 -0300

    changes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proyecto mihura</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+
+    return (
+        <div className='container'>
+
+        </div>
+    )
+}
+
+export default Home;
